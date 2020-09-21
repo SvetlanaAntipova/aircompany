2c02c1231a53cbcd324725f8da1dc8195b2c2bb7
SvetlanaAntipova <70349506+SvetlanaAntipova@users.noreply.github.com>
2020-09-21 23:55:41 +0300

    Update PassengerPlane.java

--- Java/src/main/java/Planes/PassengerPlane.java
+++ Java/src/main/java/Planes/PassengerPlane.java
@@ -4,17 +4,12 @@ import java.util.Objects;
 
 public class PassengerPlane extends Plane{
 
-    //=================FIELDS=================
-    private int passengersCapacity;
-
-    //=================CONSTRUCTORS=================
+     private int passengersCapacity;
     public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.passengersCapacity = passengersCapacity;
     }
 
-
-    //=================METHODS=================
     public int getPassengersCapacity() {
         return passengersCapacity;
     }
@@ -25,13 +20,6 @@ public class PassengerPlane extends Plane{
                 ", passengersCapacity=" + passengersCapacity +
                 '}');
     }
-
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
 
     @Override
     public boolean equals(Object o) {
